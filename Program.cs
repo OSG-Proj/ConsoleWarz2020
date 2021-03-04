a76c687abc9572e4a3519a1b78991ce2681db7ee
OSG-Proj <64036744+OSG-Proj@users.noreply.github.com>
2021-03-04 12:26:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleWarz_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("myGame", 2);
+            
+            game.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
